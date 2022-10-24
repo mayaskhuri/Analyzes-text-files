cb99dbd14d304f6b9ca9798680a3f6d93a56647c
mayaskhuri <104831434+mayaskhuri@users.noreply.github.com>
2022-10-24 18:24:56 +0400

    Create FileIndexException.java

---
+++ src/my_project/tfldf/FileIndexException.java
@@ -0,0 +1,11 @@
+package my_project.tfIdf;
+
+public class FileIndexException extends Exception{
+
+
+	private static final long serialVersionUID = 1L;
+
+	public FileIndexException(String message){
+		super(message);
+	}
+}
